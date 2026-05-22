b169bdbf1e3e431b7316a443ceea016b0976c79d
omieee <omnewi@gmail.com>
2026-05-22 14:55:36 +0530

    Updated fixes

--- BigO Notations/O of 1.py
+++ BigO Notations/O of 1.py
@@ -10,10 +10,9 @@ account_ids = [12,32,43,123,89,67] # Existing account holder account_ids
 
 print(account_ids)
 print(account_ids[3]) #constant time because it is directly going to 3rd location and pulling its value which will be 123 currently
-account_ids[4] = 90 #this will not a constant operation as the values on right needs to be shifted
-
+account_ids[4] = 90 #this will also be a constant operation as the values gets updated in place
 account_ids.append(66)# this will be a constant operation as append will add to the end no furtehr shifting to the right
-
+account_ids.insert(3,231)  # this will not be a constant operation as it will shift value to right
 print(account_ids)
 
 popped_item = account_ids.pop() #again contant as will happen to the end
