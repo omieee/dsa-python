a12985f650766e2bcce17a0c41d30655c506c940
omieee <omnewi@gmail.com>
2026-05-28 18:52:54 +0530

    Added fresh code

--- tests/test_contains_duplicate.py
+++ tests/test_contains_duplicate.py
@@ -1,4 +1,5 @@
 from Problems.contains_duplicate import contains_duplicate
 
+
 def test_contains_duplicates():
-    assert True == contains_duplicate([1,2,3,1])
+    assert contains_duplicate([1, 2, 3, 1])
--- tests/test_valid_anagram.py
+++ tests/test_valid_anagram.py
@@ -1,4 +1,5 @@
 from Problems.valid_anagram import is_anagram
 
+
 def test_anagram():
     assert True == is_anagram("anagram", "nagaram")
