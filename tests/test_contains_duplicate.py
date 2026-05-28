40cfb83dcdf44a50b97a7a06e6c2acccd3161bc9
omieee <omnewi@gmail.com>
2026-05-28 18:13:41 +0530

    Added tests

---
+++ tests/test_contains_duplicate.py
@@ -0,0 +1,4 @@
+from Problems.contains_duplicate import contains_duplicate
+
+def test_contains_duplicates():
+    assert True == contains_duplicate([1,2,3,1])
---
+++ tests/test_valid_anagram.py
@@ -0,0 +1,4 @@
+from Problems.valid_anagram import is_anagram
+
+def test_anagram():
+    assert True == is_anagram("anagram", "nagaram")
