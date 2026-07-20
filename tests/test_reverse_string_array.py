fdc5755a306405542663bdc7c78546c4083f1b0a
Om Shanker <omnewi@gmail.com>
2026-07-21 00:03:15 +0530

    Added reverse string array

--- tests/test_reverse_string_array.py
+++ tests/test_reverse_string_array.py
@@ -2,4 +2,4 @@ from Problems.reverse_string_array import reverseString
 
 
 def test_reverse_string_array() -> None:
-    assert ["a", "b", "c"] == reverseString("c", "b", "a")
+    assert ["a", "b", "c"] == reverseString(["c", "b", "a"])
