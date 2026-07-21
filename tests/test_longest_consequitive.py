0f45a275f8883d34f315c8f768a2d4cd03d49192
Om Shanker <omnewi@gmail.com>
2026-07-21 17:13:02 +0530

    Deleted also

--- tests/test_longest_consequitive.py
+++
@@ -1,6 +0,0 @@
-from Problems.longest_consequtive import longest_consecutive
-
-
-def test_longest_consequitive() -> None:
-    assert 4 == longest_consecutive([2, 20, 4, 10, 3, 4, 5])
-    assert 7 == longest_consecutive([0, 3, 2, 5, 4, 6, 1, 1])
