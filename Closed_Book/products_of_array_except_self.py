c5fe0134f6eeeb7d88e9073c97ac5536e026e192
Om Shanker <omnewi@gmail.com>
2026-07-21 11:27:35 +0530

    Updated

--- Closed_Book/products_of_array_except_self.py
+++ Closed_Book/products_of_array_except_self.py
@@ -83,7 +83,7 @@ def productExceptSelfOof1(nums: list[int]) -> list[int]:
     for i in range(1, len(nums)):
         result[i] = result[i - 1] * nums[i - 1]
     suffix_product = 1
-    for i in range(len(nums) - 2, -1, -1):
+    for i in range(len(nums) - 1, -1, -1):
         result[i] *= suffix_product
         suffix_product *= nums[i]
     return result
--- tests/test_products_of_array_except_self.py
+++ tests/test_products_of_array_except_self.py
@@ -1,6 +1,7 @@
 from Closed_Book.products_of_array_except_self import (
     productExceptSelfBrute,
     productExceptSelfOptimal,
+    productExceptSelfOof1,
 )
 
 
@@ -12,3 +13,9 @@ def test_productExceptSelfBrute():
 def test_productExceptSelfOptimal():
     assert productExceptSelfOptimal([1, 2, 3, 4]) == [24, 12, 8, 6]
     assert productExceptSelfOptimal([-1, 1, 0, -3, 3]) == [0, 0, 9, 0, 0]
+
+
+def test_productExceptSelfOof1():
+    assert productExceptSelfOof1([1, 2, 3, 4]) == [24, 12, 8, 6]
+    assert productExceptSelfOof1([-1, 1, 0, -3, 3]) == [0, 0, 9, 0, 0]
+    assert productExceptSelfOof1([2, 3]) == [3, 2]
